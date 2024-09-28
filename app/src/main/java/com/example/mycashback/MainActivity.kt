f2aadca13a55198c486caa068e0e6fccae9243a4
ant1k9 <ant1k9@protonmail.com>
2024-09-28 18:06:39 +0300

    feat: change month with swipe

--- app/src/main/java/com/example/mycashback/MainActivity.kt
+++ app/src/main/java/com/example/mycashback/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.mycashback
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycashback.adapters.CategoryGridItemAdapter
@@ -19,7 +20,10 @@ import com.example.mycashback.storage.CategoriesFileStorageProxy
 import java.time.LocalDate
 import kotlin.math.absoluteValue
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
+    private val minDistance = 200
+    private var cursorX = 0.0F
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.main, menu)
@@ -41,16 +45,21 @@ class MainActivity : ComponentActivity() {
 
     private fun buttonMonthChangedHandler(month: Int, year: Int): (View) -> Unit {
         return fun(_: View) {
-            val intent = Intent(this, MainActivity::class.java)
-            val bundle = Bundle()
-            bundle.putInt("month", month)
-            bundle.putInt("year", year)
-            intent.putExtras(bundle)
-            startActivity(intent)
-            finish()
+            monthChangedHandler(month, year)
         }
     }
 
+    private fun monthChangedHandler(month: Int, year: Int) {
+        val intent = Intent(this, MainActivity::class.java)
+        val bundle = Bundle()
+        bundle.putInt("month", month)
+        bundle.putInt("year", year)
+        intent.putExtras(bundle)
+        startActivity(intent)
+        finish()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun renderCategoryList(
         categoriesFileStorageProxy: CategoriesFileStorageProxy,
         banksFileStorageProxy: BanksFileStorageProxy
@@ -73,21 +82,64 @@ class MainActivity : ComponentActivity() {
         createButton.setOnClickListener {
             val addCategoryTextView = findViewById<TextView>(R.id.add_category_text_field)
             val category = addCategoryTextView.text
+            if (category.isBlank()) return@setOnClickListener
+
             Toast.makeText(applicationContext, "Категория $category сохранена", Toast.LENGTH_LONG)
                 .show()
             addCategoryTextView.text = ""
 
             categoriesFileStorageProxy.add(category.toString(), "")
-
             renderCategoryList(categoriesFileStorageProxy, banksFileStorageProxy)
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> cursorX = event.x
+
+            MotionEvent.ACTION_UP -> {
+                val deltaX = event.x - cursorX
+                if (deltaX.toDouble() > minDistance) {
+                    val (month, year) = previousMonthParams()
+                    monthChangedHandler(month, year)
+                }
+                if (deltaX.toDouble() < -minDistance) {
+                    val (month, year) = nextMonthParams()
+                    monthChangedHandler(month, year)
+                }
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    private fun nextMonthParams(): Pair<Int, Int> {
+        val (month, year) = currentDateMonthAndYear()
+        return Pair(
+            if (month == 12) 1 else (month + 1),
+            if (month == 12) year + 1 else year
+        )
+    }
+
+    private fun previousMonthParams(): Pair<Int, Int> {
+        val (month, year) = currentDateMonthAndYear()
+        return Pair(
+            if (month == 1) 12 else (month - 1),
+            if (month == 1) year - 1 else year
+        )
+    }
+
+    private fun currentDateMonthAndYear(): Pair<Int, Int> {
         val now = LocalDate.now()
         val month = intent.extras?.getInt("month", now.monthValue) ?: now.monthValue
         val year = intent.extras?.getInt("year", now.year.absoluteValue) ?: now.year.absoluteValue
+        return Pair(month, year)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val (month, year) = currentDateMonthAndYear()
+        val (previousMonth, previousYear) = previousMonthParams()
+        val (nextMonth, nextYear) = nextMonthParams()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.listview)
@@ -102,18 +154,12 @@ class MainActivity : ComponentActivity() {
 
         val previousMonthButton = findViewById<Button>(R.id.previous_month)
         previousMonthButton.setOnClickListener(
-            buttonMonthChangedHandler(
-                if (month == 1) 12 else (month - 1),
-                if (month == 1) year - 1 else year
-            )
+            buttonMonthChangedHandler(previousMonth, previousYear)
         )
 
         val nextMonthButton = findViewById<Button>(R.id.next_month)
         nextMonthButton.setOnClickListener(
-            buttonMonthChangedHandler(
-                if (month == 12) 1 else (month + 1),
-                if (month == 12) year + 1 else year
-            )
+            buttonMonthChangedHandler(nextMonth, nextYear)
         )
 
         val currentMonthTextView = findViewById<TextView>(R.id.current_month)
